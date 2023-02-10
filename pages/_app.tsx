80279522ec9d1f1793294ff5ada32b29362f3d2f
Vitor Petri Demantova <vitordemantova@gmail.com>
2023-02-10 14:19:55 -0300

    Home updates. 🌐

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'styles/index.scss'
-
+import MundoSvg from '@/public/mundo.svg';
+import Image from 'next/image';
 import Lenis from 'utils/scroll'
 
 import type { AppProps } from 'next/app'
@@ -11,7 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return <>
     <div className="navigation">
-      <div className="header"></div>
+      <div className="header">
+        <div className="logo"><Image src={MundoSvg} alt="World"/></div>
+        <div className="menu"><div className="icon">+</div></div>
+      </div>
       <div className="frame">
         <Component {...pageProps} />
       </div>
