52abdb34fbdeca096d36f72b84b73e3f6af0420f
Vitor Petri Demantova <vitordemantova@gmail.com>
2023-02-14 16:55:37 -0300

    🌐

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,6 @@ import Lenis from 'utils/scroll'
 import Navigation from 'components/Navigation'
 import TransitionLayout from 'components/app/TransitionLayout'
 
-// import { Noise } from '@react-three/postprocessing'
-// import { BlendFunction } from 'postprocessing'
-// import { Canvas } from '@react-three/fiber'
-
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
