cbd4904ad902a15999b0d3a4608bba5fa99fba6c
Vitor Petri Demantova <vitordemantova@gmail.com>
2023-02-14 16:58:01 -0300

    Fix. 🌐

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import 'styles/index.sass'
 import Lenis from 'utils/scroll'
 
 import Navigation from 'components/Navigation'
-import TransitionLayout from 'components/app/TransitionLayout'
 
 import type { AppProps } from 'next/app'
 
@@ -10,13 +9,11 @@ export default function App({ Component, pageProps }: AppProps) {
   Lenis
 
   return <>
-    <TransitionLayout>
-      <div className="app">
-        <Navigation />
-        <div className="content">
-          <Component {...pageProps} />
-        </div>
+    <div className="app">
+      <Navigation />
+      <div className="content">
+        <Component {...pageProps} />
       </div>
-    </TransitionLayout>
+    </div>
   </>
 }
