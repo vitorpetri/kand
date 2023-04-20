718249ee4bd62ed7a90bec8534ba04871a531e01
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2023-04-20 14:44:45 +0530

    Slicemachine 🤷‍♂️

--- .slicemachine/assets/slices/Profile/index.stories.js
+++ .slicemachine/assets/slices/Profile/index.stories.js
@@ -5,17 +5,17 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Worker","spans":[]}],"description":[{"type":"paragraph","text":"Adipisicing eu duis quis sunt exercitation esse amet.","spans":[]}]},"id":"_Default","slice_type":"profile"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Frozen","spans":[]}],"description":[{"type":"paragraph","text":"Magna anim ad esse in ullamco magna laboris aliquip reprehenderit non.","spans":[]}]},"id":"_Default","slice_type":"profile"}} />
 _Default.storyName = ''
 
-export const _Profile = () => <MyComponent slice={{"variation":"profile","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"name":"balance","last_name":"jungle","role":"stock","image":{"dimensions":{"width":4160,"height":3120},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1560457079-9a6532ccb118"}},"id":"_Profile","slice_type":"profile"}} />
+export const _Profile = () => <MyComponent slice={{"variation":"profile","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"name":"flag","last_name":"younger","role":"herd","image":{"dimensions":{"width":3168,"height":4752},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1589321578146-4c1ba445cc88"}},"id":"_Profile","slice_type":"profile"}} />
 _Profile.storyName = ''
 
-export const _Accomplishments = () => <MyComponent slice={{"variation":"accomplishments","version":"sktwi1xtmkfgx8626","items":[{"item":"usually","sub_item":"view"}],"primary":{"topic":"contain"},"id":"_Accomplishments","slice_type":"profile"}} />
+export const _Accomplishments = () => <MyComponent slice={{"variation":"accomplishments","version":"sktwi1xtmkfgx8626","items":[{"item":"ship","sub_item":"sale"}],"primary":{"topic":"tide"},"id":"_Accomplishments","slice_type":"profile"}} />
 _Accomplishments.storyName = ''
 
-export const _IndustryClients = () => <MyComponent slice={{"variation":"industryClients","version":"sktwi1xtmkfgx8626","items":[{"logo":{"link_type":"Media","name":"mock","kind":"image","url":"https://source.unsplash.com/random","size":"10kB"}}],"primary":{"topic":"cabin"},"id":"_IndustryClients","slice_type":"profile"}} />
+export const _IndustryClients = () => <MyComponent slice={{"variation":"industryClients","version":"sktwi1xtmkfgx8626","items":[{"logo":{"link_type":"Media","name":"mock","kind":"image","url":"https://source.unsplash.com/random","size":"10kB"}}],"primary":{"topic":"piece"},"id":"_IndustryClients","slice_type":"profile"}} />
 _IndustryClients.storyName = ''
 
-export const _AwardCategories = () => <MyComponent slice={{"variation":"awardCategories","version":"sktwi1xtmkfgx8626","items":[{"title":"cave","sub_title":"screen"}],"primary":{"award":"shut"},"id":"_AwardCategories","slice_type":"profile"}} />
+export const _AwardCategories = () => <MyComponent slice={{"variation":"awardCategories","version":"sktwi1xtmkfgx8626","items":[{"title":"interest","sub_title":"weather"}],"primary":{"award":"die"},"id":"_AwardCategories","slice_type":"profile"}} />
 _AwardCategories.storyName = ''
