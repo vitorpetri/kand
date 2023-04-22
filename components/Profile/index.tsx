49f11194cc04c6da1bb930ec76dfa2b25e8a8507
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2023-04-22 15:05:08 +0530

    Fixed the Profile Section in About Page

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,44 +1,75 @@
-import { image } from '@prismicio/helpers/dist/isFilled'
 import styles from './styles.module.sass'
 
 import * as prismicH from "@prismicio/helpers"
 
+import SeparatorLine from "../SeparatorLine"
+
+//map profile and accomplishments
+function profileMapping(slices) {
+    const profiles = []
+    let currentProfile = null
+
+    slices.filter((slice) => {
+        if (slice.variation === 'profile') {
+            if (currentProfile) {
+                profiles.push(currentProfile)
+            }
+            currentProfile = { ...slice, accomplishments: []}
+        } else if (slice.variation === 'accomplishments') {
+            if (currentProfile) {
+                currentProfile.accomplishments.push(slice)
+            }
+        }
+    })
+
+    if (currentProfile) {
+        profiles.push(currentProfile)
+    }
+
+    return profiles
+}
+
 export default function Profile({ data }) {
 
-    const profile = data.slices.find((slice) => slice.variation === 'profile')
-    const accomplishments = data.slices.filter((slice) => slice.variation === 'accomplishments')
+    // Profile with Accomplishments
+    const PWA = profileMapping(data.slices)
 
-    // profile.items.forEach(item => {
-    //     console.log(item)
-    // })
+    console.log(data);
 
   return (
-    <div className={styles.wrapper}>
-        <div className={styles.info}>
-        <h1 className={styles.name}>{profile.primary.name}</h1>
-        <h1 className={styles.surname}>{profile.primary.last_name}</h1>
-        <h2 className={styles.role}>{profile.primary.role}</h2>
-        <img
-            className={styles.image}
-            src={prismicH.asImageSrc(profile.primary.image, {
-              lossless: true,
-              q: 100
-            }) || ''}
-            alt={profile.name}
-          />
-        </div>
-        <div className={styles.content}>
-            {accomplishments.map((slice, index) => (
-                <ul key={index} className={styles.paragraph}>
+    <>
+    {PWA.map((profileData, index) => (
+      <>
+        {index > 0 && <SeparatorLine />}
+            <div key={index} className={styles.wrapper}>
+                <div className={styles.info}>
+                <h1 className={styles.name}>{profileData.primary.name}</h1>
+                <h1 className={styles.surname}>{profileData.primary.last_name}</h1>
+                <h2 className={styles.role}>{profileData.primary.role}</h2>
+                <img
+                    className={styles.image}
+                    src={prismicH.asImageSrc(profileData.primary.image, {
+                    lossless: true,
+                    q: 100
+                    }) || ''}
+                    alt={profileData.name}
+                />
+                </div>
+                <div className={styles.content}>
+                {profileData.accomplishments.map((slice, index) => (
+                    <ul key={index} className={styles.paragraph}>
                     <h3 className={styles.title}>{slice.primary.topic}</h3>
-                    {slice.items.map((item) => (
-                        <div>
-                            <p className={styles.text}>{item.item}</p>
-                            <p className={styles.description}>{item.sub_item}</p>
+                    {slice.items.map((item, itemIndex) => (
+                        <div key={itemIndex}>
+                        <p className={styles.text}>{item.item}</p>
+                        <p className={styles.description}>{item.sub_item}</p>
                         </div>
                     ))}
-                </ul>
-            ))}
-        </div>
-    </div>
+                    </ul>
+                ))}
+                </div>
+            </div>
+        </>
+    ))}
+  </>
 )}
