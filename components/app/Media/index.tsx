7c7d486ba75ed567edb48e272738d0e950c0e762
Vitor Petri Demantova <vitordemantova@gmail.com>
2023-04-24 15:03:39 -0300

    Clean up. 🫖

--- components/app/Media/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.sass'
-
-export default function Component() {
-  return <>
-    <main>
-      <div className={styles.style}>Component</div>
-    </main>
-  </>
-}
