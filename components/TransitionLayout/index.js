2f108c43a949c354274e2249a1b89b952fdc2dc1
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2023-06-01 10:01:41 +0530

    Achieved Dynamic Approach for transitioning the Page

--- components/TransitionLayout/index.js
+++ components/TransitionLayout/index.js
@@ -1,63 +1,65 @@
-    import { useRouter } from 'next/router'
-    import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { useRouter } from 'next/router'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 
-    import { getBoundingClientRect } from '../../utils/dom'
-    import Lenis from '../../utils/scroll'
+import { getBoundingClientRect } from '../../utils/dom'
+import Lenis from '../../utils/scroll'
 
-    const TransitionLayout = ({ children, nextUrl, setNextUrl }) => {
-        const router = useRouter()
-        const { query: { id }, asPath } = router
-        const key = id || asPath
+const TransitionLayout = ({ children, nextUrl, setNextUrl }) => {
+    const router = useRouter()
+    const { query: { id }, asPath } = router
+    const key = id || asPath
 
-        const onEnter = () => {
-            console.log("onEnter Called", key);
+    const onEnter = () => {
+        console.log("onEnter Called", key);
 
-            if (Lenis) {
-                Lenis.setScroll(0)
-            }
+        if (Lenis) {
+            Lenis.setScroll(0)
         }
+    }
 
-        const onEntered = () => {
-            console.log("onEntered Called", key);
+    const onEntered = () => {
+        console.log("onEntered Called", key);
 
-            const { hash } = window.location
+        const { hash } = window.location
 
-            if (hash) {
-                const section = document.querySelector(hash)
+        if (hash) {
+            const section = document.querySelector(hash)
 
-                if (section) {
-                    const { top } = getBoundingClientRect(section)
+            if (section) {
+                const { top } = getBoundingClientRect(section)
 
-                    if (Lenis) {
-                        Lenis.scrollTo(top)
-                    }
+                if (Lenis) {
+                    Lenis.scrollTo(top)
                 }
             }
         }
+    }
 
-        const onExited = () => {
-            console.log("onExited Called", key);
-            if (nextUrl) {
-                router.push(nextUrl);
-                setNextUrl(null);
-            }
+    const onExited = () => {
+        console.log("onExited Called", key);
+        if (nextUrl) {
+            router.push(nextUrl);
+            setNextUrl(null);
         }
+    }
 
-        return (
-            <SwitchTransition>
-                <CSSTransition
-                    classNames='page'
-                    key={key}
-                    onEnter={onEnter}
-                    onEntered={onEntered}
-                    timeout={300}
-                    onExited={onExited}
-                >
-                    <>
-                        {children}
+    const classNames = asPath.startsWith("/project/") ? "project-page" : "page";
 
-                        <style jsx global>
-                            {`
+    return (
+        <SwitchTransition>
+            <CSSTransition
+                classNames={classNames}
+                key={key}
+                onEnter={onEnter}
+                onEntered={onEntered}
+                timeout={300}
+                onExited={onExited}
+            >
+                <>
+                    {children}
+
+                    <style jsx global>
+                        {`
                     .page-enter {
                         opacity: 0;
                     }
@@ -72,15 +74,44 @@
                     }
 
                     .page-exit-active {
+                        opacity: 0;
+                        transition: opacity 400ms;
+                    }
+
+                    .project-page-enter {
+                        opacity: 0;
+                    }
+
+                    .project-page-enter-active {
                         opacity: 1;
                         transition: opacity 400ms;
                     }
-                    `}
-                        </style>
-                    </>
-                </CSSTransition>
-            </SwitchTransition>
-        )
-    }
 
-    export default TransitionLayout
+                    .project-page-exit:after {
+                        content: "";
+                        position: absolute;
+                        bottom: -200rem;
+                        left: 0;
+                        width: 100%;
+                        background: yellow;
+                        transform: scaleY(1.5);
+                        height: 100%;
+                        border-bottom-left-radius: 30px;
+                        border-bottom-right-radius: 30px;
+                        transition: all 1s cubic-bezier(1.000, 0.030, 0.440, 1.000);
+                        z-index: 9999;
+                    }
+
+                    .project-page-exit-active:after {
+                        bottom: 200rem;
+                        transition: all 400ms
+                    }
+                    `}
+                    </style>
+                </>
+            </CSSTransition>
+        </SwitchTransition>
+    )
+}
+
+export default TransitionLayout
