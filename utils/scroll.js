1dd9ced76bcdf284a2bb7df1b208275409841536
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2023-06-04 00:53:06 +0530

    Fix Border Radius issue
    Fix Border Radius Issue in Project Page > Iframe

--- utils/scroll.js
+++ utils/scroll.js
@@ -3,9 +3,9 @@ import Lenis from '@studio-freight/lenis'
 let LenisManager = null
 
 function rAF(time) {
-  LenisManager?.raf(time)
+    LenisManager?.raf(time)
 
-  requestAnimationFrame(rAF)
+    requestAnimationFrame(rAF)
 }
 
 const togglePointerEvents = (disable) => {
@@ -15,10 +15,10 @@ const togglePointerEvents = (disable) => {
     elements.forEach(element => {
         if (disable) {
             element.classList.add('disabled');
-            console.log(`Added 'disabled' class to element: `, element);
+            console.log('Added disabled class');
         } else {
             element.classList.remove('disabled');
-            console.log(`Removed 'disabled' class to element: `, element);
+            console.log('Removed disabled class');
 
         }
     });
@@ -26,15 +26,16 @@ const togglePointerEvents = (disable) => {
 
 
 if (typeof window !== 'undefined') {
-  LenisManager = new Lenis({
-    direction: 'vertical',
-    duration: 1,
-    gestureDirection: 'vertical',
-    mouseMultiplier: 0.5,
-    smooth: true,
-    smoothTouch: false,
-    touchMultiplier: 2
-  })
+    LenisManager = new Lenis({
+        direction: 'vertical',
+        duration: 1.2,
+        easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+        gestureDirection: 'vertical',
+        // mouseMultiplier: '0.5',
+        smooth: true,
+        smoothTouch: false,
+        touchMultiplier: 2
+    })
 
     let isScrolling;
 
